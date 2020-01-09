7c5caa5213cace87cf62c50eba96ef72d561bbb0
scott <848174973@qq.com>
2020-01-09 19:27:10 +0800

    mvp

---
+++ MyApplication/mvp/src/main/java/com/hpcang/mvp/delegate/FragmentMvpDelegate.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2015 Hannes Dorfmann.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.hpcang.mvp.delegate;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+import com.hpcang.mvp.MvpPresenter;
+import com.hpcang.mvp.MvpView;
+
+
+/**
+ * A delegate for Fragments to attach them to mosby mvp.
+ * <p>
+ * The following methods must be invoked from the corresponding Fragments lifecycle methods:
+ *
+ * <ul>
+ * <li>{@link #onCreate(Bundle)}</li>
+ * <li>{@link #onDestroy()}</li>
+ * <li>{@link #onPause()} </li>
+ * <li>{@link #onResume()} </li>
+ * <li>{@link #onStart()} </li>
+ * <li>{@link #onStop()} </li>
+ * <li>{@link #onViewCreated(View, Bundle)} </li>
+ * <li>{@link #onActivityCreated(Bundle)} </li>
+ * <li>{@link #onSaveInstanceState(Bundle)} </li>
+ * <li>{@link #onAttach(Activity)} </li>
+ * <li>{@link #onAttach(Context)} </li>
+ * <li>{@link #onDetach()}</li>
+ * <li></li>
+ * </ul>
+ * </p>
+ *
+ * @param <V> The type of {@link MvpView}
+ * @param <P> The type of {@link MvpPresenter}
+ * @author Hannes Dorfmann
+ * @since 1.1.0
+ */
+public interface FragmentMvpDelegate<V extends MvpView, P extends MvpPresenter<V>> {
+
+  /**
+   * Must be called from {@link #onCreate(Bundle)}
+   *
+   * @param saved The bundle
+   */
+  void onCreate(Bundle saved);
+
+  /**
+   * Must be called from {@link #()}
+   */
+  void onDestroy();
+
+  /**
+   * Must be called from {@link #onViewCreated(View, Bundle)}
+   *
+   * @param view The inflated view
+   * @param savedInstanceState the bundle with the viewstate
+   */
+  void onViewCreated(View view, @Nullable Bundle savedInstanceState);
+
+  /**
+   * Must be called from {@link #onDestroyView()}
+   */
+  void onDestroyView();
+
+  /**
+   * Must be called from {@link #onPause()}
+   */
+  void onPause();
+
+  /**
+   * Must be called from {@link #onResume()}
+   */
+  void onResume();
+
+  /**
+   * Must be called from {@link #onStart()}
+   */
+  void onStart();
+
+  /**
+   * Must be called from {@link #onStop()}
+   */
+  void onStop();
+
+  /**
+   * Must be called from {@link #onActivityCreated(Bundle)}
+   *
+   * @param savedInstanceState The saved bundle
+   */
+  void onActivityCreated(Bundle savedInstanceState);
+
+  /**
+   * Must be called from {@link #onAttach(Activity)}
+   *
+   * @param activity The activity the fragment is attached to
+   */
+  @Deprecated
+  void onAttach(Activity activity);
+
+  /**
+   * Must be called from {@link #onAttach(Context)}
+   *
+   * @param context The context the fragment is attached to
+   */
+  void onAttach(Context context);
+
+  /**
+   * Must be called from {@link #onDetach()}
+   */
+  void onDetach();
+
+  /**
+   * Must be called from {@link #onSaveInstanceState(Bundle)}
+   */
+  void onSaveInstanceState(Bundle outState);
+}
